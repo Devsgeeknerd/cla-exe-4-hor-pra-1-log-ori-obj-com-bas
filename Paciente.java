0c5da63faeefda2f09423ec6ebb3fe269b42f9ec
Paulo Vanderley <vanderley.1109@gmail.com>
2024-06-07 15:57:02 -0300

    :page_with_curl: Update file content.

--- Paciente.java
+++ Paciente.java
@@ -87,4 +87,7 @@ public class Paciente {
   public void cancelarConsulta() {
     System.out.println("Consulta cancelada para o paciente " + nome + " " + sobrenome + ".");
   }
+
+  public void consultarConsulta() {
+  }
 }
